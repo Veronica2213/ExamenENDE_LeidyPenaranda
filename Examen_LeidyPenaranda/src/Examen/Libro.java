3eb9d3d1b7b24b4e926a4597fd806fdf1d6551f4
lpenaragon <lpenaragon@educacion.navarra.es>
2025-12-17 10:49:41 +0100

    Constructor añadidob

--- Examen_LeidyPenaranda/src/Examen/Libro.java
+++ Examen_LeidyPenaranda/src/Examen/Libro.java
@@ -5,4 +5,14 @@ public class Libro {
 	String  titulo, autor, isbn;
 	boolean prestado;
 	
+	public Libro(String titulo, String autor, String isbn, boolean prestado) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.isbn = isbn;
+		this.prestado = prestado;
+	}
+	
+	
+	
 }
